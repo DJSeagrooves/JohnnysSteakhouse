c2c1c395450d8e55abe4a1bd2aed94eba27ce0ec
Hunter Segraves <segraveshunter@gmail.com>
2022-02-18 08:55:34 -0500

    cleaned up to start app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
-import Main from '../components/MainComponent';
+import React from 'react';
+import Header from './HeaderComponent';
+import Home from './HomeComponent';
+import AboutUs from './AboutUsComponent';
+import ContactUs from './ContactUsComponent';
+import Menu from './MenuComponent';
+import Footer from './FooterComponent';
 
 function App() {
+  
   return (
-    <Main />
+    <div className='App'>
+      <Header title="Johnny's Steakhouse" />
+      <Home />
+      <AboutUs />
+      <ContactUs />
+      <Menu />
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/MenuComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Menu = () => {
+  return (
+    <div>MenuComponent</div>
+  )
+}
+
+export default Menu;
